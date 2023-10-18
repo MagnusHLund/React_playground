77356fe1138ff4ae2d8b062f450dcf11645d3fd8
Magnus Lund <124877369+MagnusHLund@users.noreply.github.com>
2023-10-18 16:24:35 +0200

    Version 2

--- to-do_list_V2/src/App.js
+++ to-do_list_V2/src/App.js
@@ -1,7 +1,12 @@
 // https://react.dev/learn/updating-arrays-in-state
+// https://www.freecodecamp.org/news/how-to-use-localstorage-with-react-hooks-to-set-and-get-items/
 
-// Imports useState.
-import React, { useState } from 'react';
+// Cant get version 2 to work.
+// No errors gets output and ChatGPT says it works. 
+// Tried 2 in Opera GX and Microsoft Edge.
+
+// Imports useState and useEffect
+import React, { useState, useEffect } from 'react';
 
 // This variable is used to assign unique id's.
 let itemId = 0;
@@ -15,6 +20,19 @@ function ToDo() {
   // This represents the HTML list of toDoItems. It starts as an empty array.
   const [toDoItem, setToDo] = useState([]);
 
+  // Uses useEffect to load data from the local storage and puts them into the array, using the setToDo.
+  useEffect(() => {
+    const storedItems = localStorage.getItem('toDoItems');
+    if (storedItems) {
+      setToDo(JSON.parse(storedItems));
+    }
+  }, []);
+
+  // takes toDoItems and puts them into the local storage
+  useEffect(() => {
+    localStorage.setItem('toDoItems', JSON.stringify(toDoItem))
+  }, [toDoItem]);
+
   // Returns this HTML to the view
   return (
     <div>
@@ -24,12 +42,14 @@ function ToDo() {
       <input value={name} onChange={e => setName(e.target.value)}></input>
       {/* Button which adds an item to the to-do array, using a spread operator (...toDoItem),  
           which creates a copy of the array and makes a new one with a new item in the array. 
-          The new item in the array gets a unique id and a name value, which represents the user input. */}
+          The new item in the array gets a unique id and a name value, which represents the user input. 
+          Once the add button has been clicked, the text field gets cleared. */}
       <button onClick={() => {
         setToDo([
           ...toDoItem,
           { id: itemId++, name: name }
         ]);
+        setName("");
       }}>Add</button>
       {/* List element which includes the to-do item, along with a delete button. The button only deletes the value in the same <li> */}
       <ul>
